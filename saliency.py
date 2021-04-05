8c4f948a2102c5911c1cd025ca1422f4b0ffafcd
Sebeom Park <spark@hcil.snu.ac.kr>
2021-04-05 13:20:11 +0900

    Adding logic for 1000 sample extraction of mnist dataset

--- saliency.py
+++ saliency.py
@@ -15,147 +15,58 @@ from lib.gradients import VanillaGrad, SmoothGrad, GuidedBackpropGrad, GuidedBac
 from lib.image_utils import preprocess_image, save_as_gray_image
 from lib.labels import IMAGENET_LABELS
 
-import tarfile
 import glob
 import time
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cuda', action='store_true', default=False,
-                        help='Use NVIDIA GPU acceleration')
-    parser.add_argument('--img', type=str, default='',
-                        help='Input image path')
-    parser.add_argument('--out_dir', type=str, default='./result/grad/',
-                        help='Result directory path')
-    parser.add_argument('--n_samples', type=int, default=10,
-                        help='Sample size of SmoothGrad')
-    parser.add_argument('--tar', type=str, help="dataset tar file")
-    args = parser.parse_args()
-    args.cuda = args.cuda and torch.cuda.is_available()
-    if args.cuda:
-        print("Using GPU for acceleration")
-    else:
-        print("Using CPU for computation")
-    if args.img:
-        print('Input image: {}'.format(args.img))
-    else:
-        print('Input image: raccoon face (scipy.misc.face())')
-    print('Output directory: {}'.format(args.out_dir))
-    print('Sample size of SmoothGrad: {}'.format(args.n_samples))
-    print()
-    return args
+import tarfile
 
 
 def main():
-    args = parse_args()
-
-    if not os.path.exists(args.out_dir):
-        os.makedirs(args.out_dir)
+    cuda = False
+    n_samples = 10
+    start = time.time()
 
     target_layer_names = ['35']
     target_index = None
 
+    cwd = os.getcwd()
+    
+    # For temporary
+    #datasetDir = "datasets"
+    outputDir = "mnist_output"
+    outputNpyPath = "mnist_test.npy"
+    datasetPath = "mnist_1000_datasets.tar"
+    tmpDir = "temp_data"
+    tar_tmp = tarfile.open(datasetPath)
+    tar_tmp.extractall(tmpDir)
+    tar_tmp.close()
 
-    # for testing..
-    if args.tar :
-        cwd = os.getcwd()
-        tmp_dir = "temp_data"
+    if(os.path.exists(outputNpyPath)):
+        os.remove(outputNpyPath)
+    fileList = glob.glob(tmpDir + "/**/*.png", recursive=True)
+    print("Total dataset size : " + str(len(fileList)))
 
-        # For temporary
-        args.output_dir = "mnist_output"
-        tar_tmp = tarfile.open(args.tar)
-        tar_tmp.extractall(tmp_dir)
-        tar_tmp.close()
-        fileList = glob.glob(tmp_dir + "/**/*.png", recursive=True)
-        print("Total dataset size : " + str(len(fileList)))
-
+    with open(outputNpyPath, 'wb') as f : 
         start = time.time()
-        for filePath in fileList :
+        for filePath in fileList:
             img = cv2.imread(filePath, 1)
             img = np.float32(cv2.resize(img, (224, 224))) / 255
-            preprocessed_img = preprocess_image(img, args.cuda)
+            preprocessed_img = preprocess_image(img, cuda)
             model = vgg19(pretrained=True)
             output = model(preprocessed_img)
             pred_index = np.argmax(output.data.cpu().numpy())
             print('Prediction: {}'.format(IMAGENET_LABELS[pred_index]))
 
-            smooth_grad = SmoothGrad(       pretrained_model=model,        cuda=args.cuda,        n_samples=args.n_samples,        magnitude=True)
+            smooth_grad = SmoothGrad(
+                pretrained_model=model, cuda=cuda, n_samples=n_samples, magnitude=True)
             smooth_saliency = smooth_grad(preprocessed_img, index=target_index)
-            output_path = os.path.join(cwd,args.output_dir, filePath)
+            output_path = os.path.join(cwd, outputDir, filePath)
             print("Output Path : " + output_path)
             save_as_gray_image(smooth_saliency, output_path)
             print('Saved smooth gradient image')
-        
+            np.save(f, smooth_saliency)
+
         timeConsumed = "Time cost : " + str(time.time() - start)
-        f = open("time.txt","w")
-        f.write(timeConsumed)
-        f.close()
-
-        return
-
-
-
-
-    # Prepare input image
-    if args.img:
-        img = cv2.imread(args.img, 1)
-    else:
-        img = misc.face()
-    img = np.float32(cv2.resize(img, (224, 224))) / 255
-    preprocessed_img = preprocess_image(img, args.cuda)
-
-    model = vgg19(pretrained=True)
-    if args.cuda:
-        model.cuda()
-
-    # Prediction
-    output = model(preprocessed_img)
-    pred_index = np.argmax(output.data.cpu().numpy())
-    print('Prediction: {}'.format(IMAGENET_LABELS[pred_index]))
-
-    # Compute vanilla gradient
-    vanilla_grad = VanillaGrad(
-        pretrained_model=model, cuda=args.cuda)
-    vanilla_saliency = vanilla_grad(preprocessed_img, index=target_index)
-    save_as_gray_image(vanilla_saliency, os.path.join(args.out_dir, 'vanilla_grad.jpg'))
-    print('Saved vanilla gradient image')
-
-    # Reload preprocessed image
-    preprocessed_img = preprocess_image(img, args.cuda)
-
-    # Compute guided gradient
-    guided_grad = GuidedBackpropGrad(
-        pretrained_model=model, cuda=args.cuda)
-    guided_saliency = guided_grad(preprocessed_img, index=target_index)
-    save_as_gray_image(guided_saliency, os.path.join(args.out_dir, 'guided_grad.jpg'))
-    print('Saved guided backprop gradient image')
-
-    # Reload preprocessed image
-    preprocessed_img = preprocess_image(img, args.cuda)
-
-    # Compute smooth gradient
-    smooth_grad = SmoothGrad(
-        pretrained_model=model,
-        cuda=args.cuda,
-        n_samples=args.n_samples,
-        magnitude=True)
-    smooth_saliency = smooth_grad(preprocessed_img, index=target_index)
-    save_as_gray_image(smooth_saliency, os.path.join(args.out_dir, 'smooth_grad.jpg'))
-    print('Saved smooth gradient image')
-
-    # Reload preprocessed image
-    preprocessed_img = preprocess_image(img, args.cuda)
-
-    # Compute guided smooth gradient
-    guided_smooth_grad = GuidedBackpropSmoothGrad(
-        pretrained_model=model,
-        cuda=args.cuda,
-        n_samples=args.n_samples,
-        magnitude=True)
-    guided_smooth_saliency = guided_smooth_grad(preprocessed_img, index=target_index)
-    save_as_gray_image(guided_smooth_saliency, os.path.join(args.out_dir, 'guided_smooth_grad.jpg'))
-    print('Saved guided backprop smooth gradient image')
-
+        print(timeConsumed)
 
 if __name__ == '__main__':
     main()
